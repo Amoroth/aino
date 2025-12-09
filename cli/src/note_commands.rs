740cf7af24da4b85f5e75295313d51d99d20626d
Oskar Potaczek <Amoroth@users.noreply.github.com>
2025-12-09 14:30:16 +0000

    add naive search command

--- cli/src/note_commands.rs
+++ cli/src/note_commands.rs
@@ -3,7 +3,7 @@ use std::{collections::HashMap, io::Write};
 use crate::cli_command::{CliCommandBuilder, CliCommand, CliCommandOption};
 use crate::{print_utils, config};
 use storage::Store;
-use notes::{NoteInsert, NoteRepo};
+use notes::{NoteInsert, Note, NoteRepo};
 
 pub fn build_new_command() -> CliCommand {
     CliCommandBuilder::default()
@@ -129,6 +129,62 @@ pub fn build_get_command() -> CliCommand {
                 eprintln!("{}", print_utils::colorize(print_utils::Color::error(), "Error: Note id is required."));
             }
         }).build()
+}
+
+pub fn build_search_command() -> CliCommand {
+    CliCommandBuilder::default()
+        .set_name("search")
+        .set_description("Search for a note by a query string")
+        .add_argument("query")
+        .add_option(
+            &CliCommandOption {
+                name: "tag".to_string(),
+                short_name: Some("t".to_string()),
+                description: Some("Narrow search to a tag".to_string()),
+                is_flag: false
+            }
+        )
+        .set_action(|args: HashMap<String, Vec<String>>| {
+            let query = args.get("query").and_then(|v| v.last());
+            let tags = args.get("tag");
+
+            if query.is_none() && tags.is_none() {
+                eprintln!("{}", print_utils::colorize(print_utils::Color::error(), "Error: Query is required."));
+            }
+
+            let store = Store::new(storage::Backend::Sqlite);
+            let mut repo = NoteRepo { store };
+
+            let mut all_notes = repo.get_all();
+
+            // filter by tags
+            // if let Some(tags_list) = tags {
+            //     all_notes.retain(|n| n.tags.iter().any(|t| tags_list.contains(t)));
+            // }
+
+            // filter by query
+            if let Some(query_string) = query {
+                all_notes = slow_search(&all_notes, query_string)
+            }
+
+            if all_notes.is_empty() {
+                println!("{}", print_utils::colorize(print_utils::Color::warning(), "No notes found."));
+            } else {
+                println!("Notes:");
+                for note in all_notes {
+                    println!("{}. {}", note.id, note.content);
+                }
+            }
+        }).build()
+}
+
+// todo move this somewhere where it makes sense, like to notes crate
+// realistically, i should use something like ripgrep here, read up on Boyer–Moore string search algo and maybe implement it?
+pub fn slow_search(notes: &[Note], query: &str) -> Vec<Note> {
+    notes.iter()
+        .filter(|&note| note.content.contains(query))
+        .cloned()
+        .collect()
 }
 
 pub fn build_delete_command() -> CliCommand {
